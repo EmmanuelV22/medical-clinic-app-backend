f765139efecc37bdf591cee3e8ce15fd30457231
emmanuelv22 <emmanuel.v18@hotmail.com>
2023-09-27 21:33:21 -0300

    git config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editttttt test <code>src/App.js</code> and save to reload.
+          Editttttt tttttttttttttest <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
