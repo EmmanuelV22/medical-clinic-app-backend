64dbbcacab75b56117394de2c10c122177760d03
Valentin <valentin.rob30@outlook.com>
2023-10-18 23:08:38 -0300

    mysql

--- back/auth/route.js
+++ back/auth/route.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const { register, login, update, deleteUser, getAllUsers } = require("./auth");
-const { adminAuth } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/update").put(adminAuth, update);
-router.route("/delete").delete(adminAuth, deleteUser);
-router.route("/users").get(getAllUsers); 
+router.route("/update").put(update);
+router.route("/delete").delete(deleteUser);
+router.route("/users").get(getAllUsers);
 
 module.exports = router;
