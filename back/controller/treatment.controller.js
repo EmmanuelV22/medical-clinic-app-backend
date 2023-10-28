d3db6fca1d6a8d93fd58d2a8d0b692ac31468db5
emmanuelv22 <emmanuel.v18@hotmail.com>
2023-10-28 00:03:30 -0300

    treatment routes

---
+++ back/controller/treatment.controller.js
@@ -0,0 +1,39 @@
+const connectDB = require("../server");
+
+exports.createTreatment = async (req, res, next) => {
+    const {patient_id, confirmation , treatment , medicine , quantity , initial_date , exp_date, medical_id , patologies, surgey, finish_treatment} = req.body;
+    
+    const query =  "INSERT INTO treatment (patient_id,confirmation , treatment , medicine , quantity , initial_date , exp_date, medical_id , patologies, surgey, finish_treatment) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+
+    const values = [patient_id, confirmation , treatment , medicine , quantity , initial_date , exp_date, medical_id , patologies, surgey, finish_treatment];
+
+    connectDB.query(query, values, (error, results , fields) => {
+        if (error){
+            return res
+            .status(400)
+            .json({message: "Error creating treatment", error: error.message});
+        }
+        return res 
+        .status(201)
+        .json({ message:"Treatment successful created", treatment: results.insterId});
+    });
+};
+
+exports.updateTreatment = async (req, res, next) => {
+    const {id, patient_id, confirmation , treatment , medicine , quantity , initial_date , exp_date, medical_id , patologies, surgey, finish_treatment, updatedAt} = req.body;
+    
+    const query =  "UPDATE treatment SET patient_id=?, confirmation=? , treatment=? , medicine=? , quantity=? , initial_date=? , exp_date=?, medical_id=? , patologies=?, surgey=? , finish_treatment=?, updatedAt=? WHERE id=?";
+
+    const values = [ patient_id, confirmation , treatment , medicine , quantity , initial_date , exp_date, medical_id , patologies, surgey, finish_treatment, updatedAt , id];
+
+    connectDB.query(query, values, (error, results , fields) => {
+        if (error){
+            return res
+            .status(400)
+            .json({message: "Error updating treatment", error: error.message});
+        }
+        return res 
+        .status(200)
+        .json({ message:"Treatment successful updated", treatment: id});
+    });
+};
--- back/routes/routes.js
+++ back/routes/routes.js
@@ -5,6 +5,7 @@ const {
   deleteAgenda,
 } = require("../controller/agenda.controller");
 const { createHistory } = require("../controller/history.controller");
+const { createTreatment, updateTreatment } = require("../controller/treatment.controller");
 const router = express.Router();
 
 router.route("/agenda").get(getAgendaPatient);
@@ -12,5 +13,8 @@ router.route("/create-agenda").post(createAgenda);
 router.route("/delete-agenda").delete(deleteAgenda);
 
 router.route("/create-history").post(createHistory);
+router.route("/create-treatment").post(createTreatment)
+router.route("/update-treatment").put(updateTreatment)
+
 
 module.exports = router;
