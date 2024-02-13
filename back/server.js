9d3cdcae52710d21a810f67071b8d7d711259f0b
emmanuelv22 <emmanuel.v18@hotmail.com>
2024-02-13 16:05:24 -0300

    fix server js

--- back/server.js
+++ back/server.js
@@ -16,11 +16,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST_INTERNAL,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_HOST_INTERNAL,
 });
 
 connectToDB(pool);
