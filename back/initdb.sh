dcac4d360018142d3bb59a7243a85cd995e40ad7
emmanuelv22 <emmanuel.v18@hotmail.com>
2024-02-13 11:46:21 -0300

    render fix

--- back/initdb.sh
+++ back/initdb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export PGPASSWORD=RCmrwQF2IikvtSEdoNoSRW1Y3kxXWrC0 
-psql -U postgres_bei4_user -d postgres_bei4 -h dpg-cn5nki8l5elc73e828mg-a.oregon-postgres.render.com -p 5432 \
+psql -h dpg-cn5nki8l5elc73e828mg-a.oregon-postgres.render.com -U postgres_bei4_user postgres_bei4 \
     -c "CREATE SCHEMA clinic;"
-psql -h dpg-cn5nki8l5elc73e828mg-a.oregon-postgres.render.com -U postgres_bei4_user -p 5432 < scripts/tables.sql
+psql -h dpg-cn5nki8l5elc73e828mg-a.oregon-postgres.render.com -U postgres_bei4_user postgres_bei4 < scripts/tables.sql
 
 sh runServer.sh
 
