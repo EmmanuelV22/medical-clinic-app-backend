9456979315e6b7a0c862d08f1a85d36b93d50df3
emmanuelv22 <emmanuel.v18@hotmail.com>
2024-02-18 19:19:04 -0300

    server js credentials

--- back/server.js
+++ back/server.js
@@ -26,18 +26,19 @@ connectToDB(pool);
 
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: FRONTEND_URL,
-//   })
-// );
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true
+  })
+);
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL);
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL);
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.use(cookieParser());
 app.use("/api/auth", require("./auth/route"));
