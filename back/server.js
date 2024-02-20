630dac09d72a69598bb558609e325a287ccae400
emmanuelv22 <emmanuel.v18@hotmail.com>
2024-02-19 23:11:38 -0300

    server js

--- back/server.js
+++ back/server.js
@@ -39,11 +39,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 app.use(cookieParser());
 app.use("/", require("./auth/route"));
