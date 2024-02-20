68601f2ddb7d51789739de59a8bc0b5e90d9d3e1
emmanuelv22 <emmanuel.v18@hotmail.com>
2024-02-19 21:49:04 -0300

    index js front

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./styles/index.css";
 import Layout from "./Layout";
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Layout />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
